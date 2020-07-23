4d4ff700ab595ec5f65ac5ff4e61248d9873d370
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-22 20:52:36 -0600

    Added breaks to switch function to prevent createEngineer function from running twice each call.

--- Develop/app.js
+++ Develop/app.js
@@ -72,10 +72,13 @@ function createTeam() {
         switch(response.employeeType) {
             case "Engineer":
                 createEngineer();
+                break;
             case "Intern":
                 createIntern();
+                break;
             default:
                 buildTeam();
+                break;
         }
     })
 }
@@ -88,24 +91,3 @@ function buildTeam() {
 }
 
 createManager();
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
